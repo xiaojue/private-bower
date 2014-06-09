4399b5d4eb2a2440a6948bb222d1c7a070485eb5
xiaojue <designsor@gmail.com>
2014-06-09 13:46:10 +0800

    update config

--- site/private-bower.js
+++ site/private-bower.js
@@ -1,18 +1,14 @@
-angular.module('PrivateBower', ['ngAnimate'])
-    .controller('mainController', function($scope, $http) {
-        angular.extend($scope, {
-            packages: null,
-            error: false
-        });
-
-        init();
-        function init() {
-            $http.get('/packages')
-                .success(function(packages) {
-                    $scope.packages = packages;
-                })
-                .error(function(error) {
-                    $scope.error = true;
-                });
-        }
-    });
+angular.module('PrivateBower', ['ngAnimate']).controller('mainController', function($scope, $http) {
+ angular.extend($scope, {
+     packages: null,
+     error: false
+ });
+ function init() {
+     $http.get('/packages').success(function(packages) {
+           $scope.packages = packages;
+     }).error(function(error) {
+           $scope.error = true;
+     });
+ }
+ init();
+});
